01fd4c09087ca1bd7fdf6bb0ae65b933bfb948ee
Evelyn <strazos@protonmail.com>
2022-08-25 16:06:33 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# RDSC
-Roblox Death Sound Changer
+RDSC - Roblox Death Sound Changer
+___________________________________________________
+Fixes the death sound that Roblox implemented as of version-9045f70ea522489c, although you can also add your own sounds.
+___________________________________________________
+This project is licensed under MIT, which allows anybody to modify it, whether publicly or privately, if they wish.
+___________________________________________________
+Requirements: 
+-Python 3.9.6 (https://www.python.org/downloads/) (If on Windows, must have access to PATH)
+___________________________________________________
+How to run:
+Run the "run-mac.sh" file if you are on Mac, or run "run-windows.bat" if you are on Windows.
+___________________________________________________
+Release log:
+
+Release 1 (7.26.2022):
+-Initial release
+
+Release 1.1 (8.25.2022):
+-Now supports clients on znext channel
+
+Release 1.2A (8.25.2022):
+-Preliminary Mac support
+___________________________________________________
---
+++ script.py
@@ -0,0 +1,77 @@
+#[
+# Roblox Death Sound Changer
+# Creator: devoid#5177 
+# Last update: August 25 2022
+#]
+
+##===========================================================================================##
+## Import libraries
+
+import urllib.request
+import shutil
+import json
+import os
+import platform
+from os import path
+
+##===========================================================================================##
+## Platform checking
+
+Platform = ""
+if platform.system() == "Windows":
+    Platform = "Windows"
+elif platform.system() == "Darwin":
+    Platform = "Mac"
+else:
+    Platform = "Unsupported"
+    print('You are running this script on an unsupported OS: '+str(platform.system()))
+    exit()
+
+##===========================================================================================##
+## Fetch versions from APIs (Windows only)
+
+LiveRequest = urllib.request.urlopen('https://clientsettings.roblox.com/v2/client-version/WindowsPlayer').read()
+BetaRequest = urllib.request.urlopen('https://clientsettings.roblox.com/v2/client-version/WindowsPlayer/channel/zfeatureqt5.15').read()
+LiveVersion = json.loads(LiveRequest)
+BetaVersion = json.loads(BetaRequest)
+if Platform == "Windows":
+    if LiveRequest and not BetaRequest:
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Cannot fetch znext version. Skipping.')
+    elif not LiveRequest:
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Cannot contact Roblox API to fetch live version.')
+        exit()
+
+##===========================================================================================##
+## Copy the sound file
+
+if Platform == "Windows":
+    LiveLocation = path.expandvars('C:/Users/'+os.path.split(os.path.expanduser('~'))[-1]+'/AppData/Local/'+'Roblox/Versions/'+LiveVersion['clientVersionUpload']+r'/content/sounds')
+    BetaLocation = path.expandvars('C:/Users/'+os.path.split(os.path.expanduser('~'))[-1]+'/AppData/Local/'+'Roblox/Versions/'+BetaVersion['clientVersionUpload']+r'/content/sounds')
+
+    if os.path.isdir(LiveLocation) == False and os.path.isdir(BetaLocation) == False:
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Roblox directory not found. Is Roblox installed?')
+        exit()
+    if os.path.isdir(LiveLocation) == True: 
+        File = os.path.abspath("ouch.ogg")
+        shutil.copy2(File, LiveLocation)
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Found live client and copied sound file.')
+    if os.path.isdir(BetaLocation) == True: 
+        File = os.path.abspath("ouch.ogg")
+        shutil.copy2(File, BetaLocation)
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Found beta client and copied sound file.')
+    print('Enjoy!')
+    input("Press Enter to continue...")
+elif Platform == "Mac":
+    Location = path.expandvars('/Applications/Roblox.app/Contents/Resources/content/sounds')   
+    if os.path.isdir(Location) == False: 
+        os.system('cls' if os.name=='nt' else 'clear')
+        print('Roblox directory not found. Is Roblox installed?')
+        exit()
+    File = os.path.abspath("ouch.ogg")
+    shutil.copy2(File, Location)
+    print('Found client and copied sound file. Enjoy!')
